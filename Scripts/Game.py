968118f18529139b7804fc5592ab6bf5d79c61bd
TheVaxStudio <rodrigomassanori@hotmail.com>
2023-09-08 15:06:45 -0300

    all

--- Scripts/Game.py
+++ Scripts/Game.py
@@ -1,23 +1,11 @@
-import pygame
-from pygame import OPENGL, DOUBLEBUF
-from Scripts.MapGenerator import MapGenerator
+import rubato as Rub
 
 class Game:
     @staticmethod
     def Init():
         Width = 1440
         Height = 690
-        Title = "Vaxomon Eternal Nightmares"
-        GameIsRunning = True
-        pygame.init()
-        pygame.display.set_caption(Title)
-        Display = (Width, Height)
-        pygame.display.set_mode(Display, DOUBLEBUF | OPENGL)
-        Map = MapGenerator()
-        while GameIsRunning:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-
+        ScreenFull = False
+        Rub.init(name = "Vaxomon Eternal Nightmares", res = (Width, Height), fullscreen = ScreenFull)
+        Rub.begin()
 Game.Init()
