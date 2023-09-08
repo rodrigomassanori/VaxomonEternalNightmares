e0fd40ce3239a8c3c38b455011c7e5c2a1d4b126
TheVaxStudio <rodrigomassanori@hotmail.com>
2023-09-08 11:34:59 -0300

    all

--- Scripts/MapGenerator.py
+++ Scripts/MapGenerator.py
@@ -1,2 +1,8 @@
 class MapGenerator:
-    pass
+    @staticmethod
+    def LoadMap():
+        TileX = 64
+        TileY = 64
+        for x in TileX:
+            for y in TileY:
+                pass
