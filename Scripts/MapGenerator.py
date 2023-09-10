85a76b73dbd85af511479c37f6e833cfb52ce810
TheVaxStudio <rodrigomassanori@hotmail.com>
2023-09-10 08:53:18 -0300

    all

--- Scripts/MapGenerator.py
+++ Scripts/MapGenerator.py
@@ -1,27 +1,28 @@
 class MapGenerator:
     TileSetX = 64
     TileSetY = 64
-    @staticmethod
-    def InitMap(scrn):
-        img0 = ("Tilesets/MissingTexture.png", scrn)
-        img1 = ("Tilesets/Flor.png", scrn)
-        img2 = ("Tilesets/GrassTop.png", scrn)
 
-    def load(filename, scrn):
-        imgMap = (filename, scrn)
+    @staticmethod
+    def InitMap():
+        img0 = "Tilesets/MissingTexture.png"
+        img1 = "Tilesets/Flor.png"
+        img2 = "Tilesets/GrassTop.png"
+
+    def load(filename):
+        imgMap = filename
         mapSize = imgMap.image.get_size()
         mat = [[0] * mapSize[1] for _ in range(mapSize[0])]
-        
+
         for i in range(0, mapSize[0]):
             for j in range(0, mapSize[1]):
                 color = imgMap.image.get_at((i, j))
 
                 if color == (0, 0, 0):
                     mat[i][j] = 0
-                    
+
                 elif color == (255, 255, 255):
                     mat[i][j] = 1
-                    
+
                 else:
                     mat[i][j] = -1
 
@@ -29,10 +30,10 @@ class MapGenerator:
         for i in range(0, self.mapSize[0]):
             for j in range(0, self.mapSize[1]):
                 if self.mat[i][j] == 0:
-                    self.img1.draw(i * self.TileSetX, j*self.TileSetY)
-                    
+                    self.img1.draw(i * self.TileSetX, j * self.TileSetY)
+
                 elif self.mat[i][j] == 1:
                     self.img2.draw(i * self.TileSetX, j * self.TileSetY)
-                    
+
                 else:
                     self.img0.draw(i * self.TileSetX, j * self.TileSetY)
